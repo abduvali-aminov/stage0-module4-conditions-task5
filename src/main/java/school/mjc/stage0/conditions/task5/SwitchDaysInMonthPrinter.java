7e8e8c56cdb91e3b1a2727ae86411cc41eb4dfd6
Abduvali Aminov <aminov.abduvali.98@gmail.com>
2022-06-30 02:00:04 +0500

    fixed

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -8,23 +8,21 @@ public class SwitchDaysInMonthPrinter {
                 break;
             case 12:
             case 1:
-            case 2:
-                System.out.println("Winter");
-                break;
             case 3:
-            case 4:
             case 5:
-                System.out.println("Spring");
-                break;
-            case 6:
             case 7:
             case 8:
-                System.out.println("Summer");
-                break;
-            case 9:
             case 10:
+                System.out.println(31);
+                break;
+            case 4:
+            case 6:
+            case 9:
             case 11:
-                System.out.println("Autumn");
+                System.out.println(30);
+                break;
+            case 2:
+                System.out.println(28);
                 break;
         }
     }
